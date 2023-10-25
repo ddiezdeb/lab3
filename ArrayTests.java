97011da8bd0123381ee05c55963a1558f3557883
ddiezdeb <147003235+ddiezdeb@users.noreply.github.com>
2023-10-25 15:04:53 -0700

    UpdateLab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+    @Test
+  public void testReversedThreeInputs() {
+    int[] input1 = { 1,2,3};
+    assertArrayEquals(new int[]{ 3,2,1}, ArrayExamples.reversed(input1));
+  } //reversed(int[]) method returns array with every value set to 0
+
+    @Test
+  	public void testReverseInPlaceThreeInputs() {
+    int[] input1 = { 1,2,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3,2,1 }, input1);
+	} // reverseInPlace creates palindrome replacing first half with second half
 }
